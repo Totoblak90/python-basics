6b8d9ee3a7c8c0a2ccfab310cd175d2ac9723b33
Totoblak90 <tobias.blaksley@hotmail.com>
2023-11-02 12:25:29 -0300

    saved the high score in a text file to make it persistant

--- snake/scoreboard.py
+++ snake/scoreboard.py
@@ -8,7 +8,9 @@ class Scoreboard(Turtle):
     def __init__(self) -> None:
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt", mode="r") as f:
+            content = f.read()
+            self.high_score = int(content)
         self.color("white")
         self.hideturtle()
         self.penup()
@@ -26,6 +28,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as f:
+                f.write(str(self.high_score))
         self.score = 0
         self.update_scoreboard()
 
