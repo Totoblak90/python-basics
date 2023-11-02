f51074591e6eed2679fc00ad124c01abaecf4dbc
Totoblak90 <tobias.blaksley@hotmail.com>
2023-11-02 19:47:39 -0300

    fixed a variable name

--- converter/main.py
+++ converter/main.py
@@ -8,11 +8,11 @@ window.config(padx=20, pady=20)
 def calculate_miles():
     try:
         miles_to_km = round(float(miles_entry.get()) * 1.6, 5)
-        miles_value_label.config(text=miles_to_km)
+        km_values_label.config(text=miles_to_km)
         error_label.config(text="")
         error_label.grid_remove()
     except:
-        miles_value_label.config(text="NaN")
+        km_values_label.config(text="NaN")
         error_label.config(text="Only numbers are accepted")
         error_label.grid(column=1, row=4)
         pass
@@ -27,8 +27,8 @@ miles_text_label.grid(column=2, row=0)
 is_equal_to_label = Label(text="is equal to", padx=5, pady=5)
 is_equal_to_label.grid(column=0, row=1)
 
-miles_value_label = Label(text=0, padx=5, pady=5)
-miles_value_label.grid(column=1, row=1)
+km_values_label = Label(text=0, padx=5, pady=5)
+km_values_label.grid(column=1, row=1)
 
 km_text_label = Label(text="Km", padx=5, pady=5)
 km_text_label.grid(column=2, row=1)
